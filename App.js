28ca00a81339eda35fd408df57d9d1bd4658bdc7
Darlan Bomfim <darlanbs05@gmail.com>
2023-10-18 22:06:58 -0300

    Criação do theme para estilização

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import { NavigationContainer } from "@react-navigation/native";
 import React from "react";
 import HeaderStack from "./src/routes/route";
+import "styled-components";
+import { ThemeProvider } from "styled-components";
+import Theme from "./src/theme/theme";
 
 function App() {
   return (
-    <NavigationContainer>
-      <HeaderStack />
-    </NavigationContainer>
+    <ThemeProvider theme={Theme}>
+      <NavigationContainer>
+        <HeaderStack />
+      </NavigationContainer>
+    </ThemeProvider>
   );
 }
 
---
+++ src/theme/theme.js
@@ -0,0 +1,21 @@
+const Theme = {
+  color: {
+    background: "#323238",
+    primary: {
+      main: "#323238",
+      contrastText: "#fff",
+    },
+  },
+
+  border: {
+    radius: "15px",
+    style: "2px solid #736681",
+  },
+
+  font: {
+    small: "1rem",
+    default: "1.5rem",
+  },
+};
+
+export default Theme;
